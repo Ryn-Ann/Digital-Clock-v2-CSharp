0b1ae7114d5378fb8efb725b82370e32735b9c27
Ryna K <safaryna79@gmail.com>
2023-02-06 19:50:54 +0800

    Add project files.

---
+++ Digital Clock v2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Digital_Clock_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("BankGothic Md BT", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(8, 16);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(280, 67);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "HH:MM";
+            // 
+            // labelSeconds
+            // 
+            this.labelSeconds.AutoSize = true;
+            this.labelSeconds.Font = new System.Drawing.Font("BankGothic Md BT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSeconds.Location = new System.Drawing.Point(266, 27);
+            this.labelSeconds.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSeconds.Name = "labelSeconds";
+            this.labelSeconds.Size = new System.Drawing.Size(67, 34);
+            this.labelSeconds.TabIndex = 1;
+            this.labelSeconds.Text = "SS";
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Font = new System.Drawing.Font("BankGothic Md BT", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDay.Location = new System.Drawing.Point(121, 94);
+            this.labelDay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(369, 67);
+            this.labelDay.TabIndex = 2;
+            this.labelDay.Text = "Saturday";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("BankGothic Md BT", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(127, 151);
+            this.labelDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(386, 40);
+            this.labelDate.TabIndex = 3;
+            this.labelDate.Text = "Month Date Year";
+            // 
+            // timerClock
+            // 
+            this.timerClock.Interval = 1000;
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 221);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelSeconds);
+            this.Controls.Add(this.labelTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Digital Clock v2 - Noor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelSeconds;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Timer timerClock;
+    }
+}
+
---
+++ Digital Clock v2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+ * Digital Clock v2 
+ * A simple digital clock written in C# in VS using Windows Form Apps
+ *  
+ * Noor Kamaruddin Feb 6, 2023
+ */
+
+namespace Digital_Clock_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            // Contents of the digital clock
+            labelTime.Text = DateTime.Now.ToString("HH:mm");
+            labelSeconds.Text = DateTime.Now.ToString("ss");
+            labelDate.Text = DateTime.Now.ToString("d MMMM yyyy");
+            labelDay.Text = DateTime.Now.ToString("dddd");
+
+            // Fixed the position of the Seconds label
+            labelSeconds.Location = new Point(labelTime.Location.X + labelTime.Width - 15, labelSeconds.Location.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerClock.Start();
+        }
+    }
+}
